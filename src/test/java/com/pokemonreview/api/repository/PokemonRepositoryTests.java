18e90c48147617c88ebe6f2cf3a7eb0c9b08219b
nami2528 <nami2528@gmail.com>
2023-11-20 16:17:08 +0900

    Exception add Advice class

--- src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -53,12 +53,13 @@ public class PokemonRepositoryTests {
 
         Assertions.assertThat(pokemonList).isNotNull();
         Assertions.assertThat(pokemonList.size()).isEqualTo(2);
+        Assertions.assertThat(pokemonList.get(0).getName()).isEqualTo("Pikachu");
     }
 
     @Test
     public void PokemonRepository_FindById_ReturnPokemon() {
         Pokemon pokemon = Pokemon.builder()
-                .name("pikachu")
+                .name("Pikachu")
                 .type(PokemonType.ELECTRIC).build();
 
         pokemonRepository.save(pokemon);
@@ -79,12 +80,12 @@ public class PokemonRepositoryTests {
 
         pokemonRepository.save(pokemon);
 
-        Pokemon pokemonList = pokemonRepository
+        Pokemon existPokemon = pokemonRepository
                 .findByType(pokemon.getType())
                 .get();
 
-        Assertions.assertThat(pokemonList).isNotNull();
-				Assertions.assertThat(pokemon.getType().name()).isEqualTo(PokemonType.ELECTRIC.name());
+        Assertions.assertThat(existPokemon).isNotNull();
+        Assertions.assertThat(pokemon.getType().name()).isEqualTo(PokemonType.ELECTRIC.name());
     }
 
     @Test
